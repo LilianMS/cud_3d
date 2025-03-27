e802e129895153e517cf6df4549264be389e0592
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2025-03-27 13:58:59 -0300

    update for main

--- src/utils.c
+++ src/utils.c
@@ -5,7 +5,8 @@ void	cub_error(const char *str, t_cub3d *mapdata)
 	ft_putstr_fd("Error!\n", 2);
 	ft_putstr_fd((char *)str, 2);
 	ft_putstr_fd("\n", 2);
-	cub_clean(mapdata);
+	if (mapdata)
+		cub_clean(mapdata);
 	exit(EXIT_FAILURE);
 }
 
